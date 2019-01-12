133b6cd0c10c47c37e5355e7673bc2362dc42aaa
nexuzz <nexuzz@outlook.com>
2019-01-12 17:30:43 -0300

    Gui Reloj Pruebas de diseño

--- PrincipalReloj/Form1.Designer.cs
+++ PrincipalReloj/Form1.Designer.cs
@@ -29,22 +29,18 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lblDataTime = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Temporizador = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.btn_iniciar = new System.Windows.Forms.Button();
+            this.lbl_Mes = new System.Windows.Forms.Label();
+            this.lbl_anio = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDataTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblDataTime
-            // 
-            this.lblDataTime.AutoSize = true;
-            this.lblDataTime.Font = new System.Drawing.Font("DS-Digital", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDataTime.Location = new System.Drawing.Point(4, 16);
-            this.lblDataTime.Name = "lblDataTime";
-            this.lblDataTime.Size = new System.Drawing.Size(87, 36);
-            this.lblDataTime.TabIndex = 0;
-            this.lblDataTime.Text = "HORA";
-            this.lblDataTime.Click += new System.EventHandler(this.label1_Click);
             // 
             // Temporizador
             // 
@@ -54,46 +50,139 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 10);
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(329, 36);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Parar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btn_iniciar
             // 
-            this.btn_iniciar.Location = new System.Drawing.Point(310, 40);
+            this.btn_iniciar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_iniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_iniciar.ForeColor = System.Drawing.Color.White;
+            this.btn_iniciar.Location = new System.Drawing.Point(329, 65);
             this.btn_iniciar.Name = "btn_iniciar";
             this.btn_iniciar.Size = new System.Drawing.Size(75, 23);
             this.btn_iniciar.TabIndex = 2;
             this.btn_iniciar.Text = "Iniciar";
-            this.btn_iniciar.UseVisualStyleBackColor = true;
+            this.btn_iniciar.UseVisualStyleBackColor = false;
             this.btn_iniciar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl_Mes
+            // 
+            this.lbl_Mes.AutoSize = true;
+            this.lbl_Mes.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Mes.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Mes.ForeColor = System.Drawing.Color.White;
+            this.lbl_Mes.Location = new System.Drawing.Point(161, 124);
+            this.lbl_Mes.Name = "lbl_Mes";
+            this.lbl_Mes.Size = new System.Drawing.Size(56, 42);
+            this.lbl_Mes.TabIndex = 3;
+            this.lbl_Mes.Text = "00";
+            // 
+            // lbl_anio
+            // 
+            this.lbl_anio.AutoSize = true;
+            this.lbl_anio.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_anio.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_anio.ForeColor = System.Drawing.Color.White;
+            this.lbl_anio.Location = new System.Drawing.Point(160, 178);
+            this.lbl_anio.Name = "lbl_anio";
+            this.lbl_anio.Size = new System.Drawing.Size(94, 42);
+            this.lbl_anio.TabIndex = 4;
+            this.lbl_anio.Text = "0000";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(376, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(46, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 42);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Año:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(44, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 42);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Mes:";
+            // 
+            // lblDataTime
+            // 
+            this.lblDataTime.AutoSize = true;
+            this.lblDataTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblDataTime.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataTime.ForeColor = System.Drawing.Color.White;
+            this.lblDataTime.Location = new System.Drawing.Point(42, 36);
+            this.lblDataTime.Name = "lblDataTime";
+            this.lblDataTime.Size = new System.Drawing.Size(241, 63);
+            this.lblDataTime.TabIndex = 0;
+            this.lblDataTime.Text = "00:00:00";
+            this.lblDataTime.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 72);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImage = global::PrincipalReloj.Resource1.asd;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(412, 396);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_anio);
+            this.Controls.Add(this.lbl_Mes);
             this.Controls.Add(this.btn_iniciar);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblDataTime);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reloj Simple";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblDataTime;
         private System.Windows.Forms.Timer Temporizador;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.Label lbl_Mes;
+        private System.Windows.Forms.Label lbl_anio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDataTime;
     }
 }
 
--- PrincipalReloj/Form1.cs
+++ PrincipalReloj/Form1.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace PrincipalReloj
 {
     public partial class Form1 : Form
     {
+        DateTime Tiempo = new DateTime();
+        DateTime anio = new DateTime();
+
         public Form1()
         {
             InitializeComponent();
+            Temporizador.Start();
+            
         }
 
         private void label1_Click(object sender, EventArgs e){}
         private void marcarHora()
         {
-            lblDataTime.Text = DateTime.Now.ToString("G");
+            lblDataTime.Text = DateTime.Now.Hour.ToString("G") + ":" + DateTime.Now.Minute.ToString("G") + ":" + DateTime.Now.Second.ToString("G");
+            lbl_Mes.Text = Tiempo.Month.ToString("d2") +" "+ DateTime.Now.ToString("MMMM").ToUpper();//
+
+            lbl_anio.Text = DateTime.Now.Year.ToString("G"); ; //Tiempo.Year.ToString();
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -28,6 +37,11 @@ namespace PrincipalReloj
         private void button2_Click(object sender, EventArgs e)
         {
             Temporizador.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
